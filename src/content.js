51026431b122ff4e7bf926b64e752789f5d49fa9
Albert Sheu <albert.sheu@gmail.com>
2016-02-17 22:35:12 -0800

    Minimal boilerplate from the webpack tutorial

---
+++ src/content.js
@@ -0,0 +1,5 @@
+const content = "This is a constant string";
+
+const modifier = (s) => s + " OKAY! ";
+
+module.exports = modifier(content);
---
+++ webpack.config.dev.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'cheap-eval-source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/dev-server',
+    './src/index'
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    loaders: [
+      {test: /\.css$/, loaders: ['style', 'css']},
+      {test: /\.jsx?$/, loaders: ['babel?cacheDirectory'], include: './src/'}
+    ]
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+};
