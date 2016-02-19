46b87fff835f37903885a14622de1b50388a11ba
Albert Sheu <albert.sheu@gmail.com>
2016-02-18 18:25:38 -0800

    Use client id as the channel for return values

--- src/pages/ReadlineMain.js
+++ src/pages/ReadlineMain.js
@@ -24,19 +24,19 @@ export default class ReadlineMain extends React.Component {
   componentDidMount() {
     // TODO: security for all these channels
     // TODO: allow dashes in channel ids
-    let clientChannelId = this.props.pageId + 'Client';
+    let sendChannelId = this.props.pageId;
+    this.sendChannel = new PageChannel('http://' + SERVER_NAME + '/channel/', sendChannelId);
+
+    let clientChannelId = this.sendChannel.sessionId;
     this.recvChannel = new PageChannel('http://' + SERVER_NAME + '/channel/', clientChannelId);
     this.recvChannel.listen();
 
     this.recvChannel.onMessage((message) => {
       console.log('Appending message:', message);
       this.setState({
-        items: items.concat([message])
+        items: this.state.items.concat([message])
       });
     });
-
-    let sendChannelId = this.props.pageId;
-    this.sendChannel = new PageChannel('http://' + SERVER_NAME + '/channel/', sendChannelId);
     this.sendChannel.sendStart();
   }
 
@@ -73,7 +73,7 @@ export default class ReadlineMain extends React.Component {
   render() {
     return (
       <section className="container">
-        <h1>ReadlineIO Main</h1>
+        <h1>ReadlineIO</h1>
         { this.state.items.map((item) => (this.renderItem(item))) }
       </section>
     );
