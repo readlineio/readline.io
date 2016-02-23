6f2aecd6464633120581170ac4a90eca9455fc4d
Albert Sheu <albert.sheu@gmail.com>
2016-02-22 21:28:23 -0800

    Move BaseStyles into constants

---
+++ src/constants/Constants.js
@@ -0,0 +1,28 @@
+
+const containerWidth = 960;
+
+const BaseStyles = {
+
+  card: {
+    backgroundColor: 'white',
+    boxShadow: '0 1px 1px rgba(0,0,0,0.12), 0 1px 1px rgba(0,0,0,0.24)'
+  },
+
+  smallText: {
+    fontSize: 12
+  },
+
+  mediumText: {
+    fontSize: 16
+  },
+
+  largeText: {
+    fontSize: 20,
+    fontWeight: 500
+  }
+
+}
+
+export {
+  BaseStyles
+};
